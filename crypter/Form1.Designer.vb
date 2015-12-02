10d0ecbb4f6501b0109556c323938d9b27f0cf31
rjbarna <rjbarna@umich.edu>
2015-12-02 00:21:41 -0500

    Fix ui big / push screenshots

--- crypter/Form1.Designer.vb
+++ crypter/Form1.Designer.vb
@@ -149,7 +149,7 @@ Partial Class Form1
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(180, 20)
         Me.Label6.TabIndex = 11
-        Me.Label6.Text = "Text you want to encrpyt"
+        Me.Label6.Text = "Text you want to encrypt"
         '
         'TextBox1
         '
